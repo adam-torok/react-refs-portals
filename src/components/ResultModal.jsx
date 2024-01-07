baa0d405ddfb783625188a4e31292420a6c390be
Török Ádám <adam-torok@outlook.hu>
2024-01-07 19:52:27 +0100

    feature: result modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,12 +1,28 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const resultModal =  forwardRef(function ResultModal({ result, targetTime }, ref) {
+const resultModal =  forwardRef(function ResultModal({ onReset, targetTime, remainingTime }, ref) {
+    const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                console.log('opening the modal');
+                dialog.current.showModal();
+            }
+        }
+    })
+
     return (
-        <dialog ref={ref} className="result-modal">
-            <h2>You have {result ? 'won' : 'lost'}</h2>
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+            <h2>You have {!userLost ? 'won' : 'lost'}</h2>
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was {targetTime} seconds.</p>
-            <p>You stopped the timer with <strong>X seconds left</strong></p>
-            <form method="dialog">
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
